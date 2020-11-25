f9f924fc4cf594665b41d71f11ff512399debaf1
*Frezo* <wilczewski.dominik@gmail.com>
2020-11-25 22:42:04 +0100

    Create EasyPass.py

---
+++ EasyPass.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import Menu
+from tkinter import ttk
+import os
+import datetime
+from tkinter import messagebox
+from cryptography.fernet import Fernet
